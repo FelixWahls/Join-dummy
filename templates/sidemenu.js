d8312bcda271de1c17c08b894850ed8e5327c081
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-03-26 19:54:47 +0100

    template responsiv

---
+++ templates/sidemenu.js
@@ -0,0 +1,23 @@
+function submenuInit(){
+    let submenu = document.querySelector('submenu');
+    submenu.innerHTML = '';
+    submenu.innerHTML = ``;
+}
+
+/*
+                <div class="submenu">
+					<div class="submenuItem">
+						<span class="submenuText">Help</span>
+					</div>
+					<div class="submenuItem">
+						<span class="submenuText">Legal Notice</span>
+					</div>
+					<div class="submenuItem">
+						<span class="submenuText">Privacy Policy</span>
+					</div>
+					<div class="submenuItem">
+						<span class="submenuText">Log out</span>
+					</div>
+				</div>
+
+*/
