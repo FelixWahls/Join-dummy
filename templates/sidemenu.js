dd8f06d99f71b2bd45b7fbca495c2f12ba134528
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-03-27 09:08:56 +0100

    template responsiv

--- templates/sidemenu.js
+++ templates/sidemenu.js
@@ -3,13 +3,17 @@
         if(window.innerWidth <= 1100){
             submenu.innerHTML = '';
         submenu.innerHTML = `
-        <div class="submenu">
-                        <div class="submenuItem">
+            <div class="submenu">
+                <a href="../help/help.html">
+                    <div class="submenuItem">
                             <span class="submenuText">Help</span>
-                        </div>
-                        <div class="submenuItem">
-                            <span class="submenuText">Legal Notice</span>
-                        </div>
+                    </div>
+                </a>
+                <a href="../legalNotice/legalNotice.html"  target="_blank">
+                    <div class="submenuItem">
+                        <span class="submenuText">Legal Notice</span>
+                    </div>
+                </a>    
                         <div class="submenuItem">
                             <span class="submenuText">Privacy Policy</span>
                         </div>
