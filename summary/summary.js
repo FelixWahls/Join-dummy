a6ccc70042255b8e5cd0b6c62f11d33fcb0c9817
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-04-12 23:58:32 +0200

    added render function

--- summary/summary.js
+++ summary/summary.js
@@ -1,14 +1,15 @@
-function render(){
+async function render(){
+    allTasks = await getItem('allTasks');
     for (let i = 0; i < allTasks.length; i++) {
         const tasks = allTasks[i];
-        
+        document.querySelector('.summaryToDo').innerHTML = 10;
+        document.querySelector('.summaryDone').innerHTML = 20;
+        document.querySelector('.summaryUrgent').innerHTML = 30;
+        document.querySelector('.summaryDate').innerHTML = '01.01.2025';
+        document.querySelector('.summaryAllTasks').innerHTML = i+1;
+        document.querySelector('.summaryProgress').innerHTML = 50;
+        document.querySelector('.summaryFeedback').innerHTML = 60;
+        document.querySelector('#greeting-name').innerHTML = 'AAA BBB';
     }
-    document.querySelector('.summaryToDo').innerHTML = 10;
-    document.querySelector('.summaryDone').innerHTML = 20;
-    document.querySelector('.summaryUrgent').innerHTML = 30;
-    document.querySelector('.summaryDate').innerHTML = '01.01.2025';
-    document.querySelector('.summaryAllTasks').innerHTML = 40;
-    document.querySelector('.summaryProgress').innerHTML = 50;
-    document.querySelector('.summaryFeedback').innerHTML = 60;
-    document.querySelector('#greeting-name').innerHTML = 'AAA BBB';
+    
 }
