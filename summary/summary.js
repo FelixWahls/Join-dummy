a351b310eb9792d28c67a1499fc346ffc9002ee6
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-04-13 01:43:19 +0200

    added render function

--- summary/summary.js
+++ summary/summary.js
@@ -1,41 +1,54 @@
 async function render() {
-	allTasks = await getItem('allTasks');
-	let a = 0;
-	let b = 0;
-	let c = 0;
-	let d = 0;
-	let u = 0;
-	let date = 0;
+  allTasks = await getItem("allTasks");
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  let d = 0;
+  let u = 0;
+  let date = 0;
 
-	for (let i = 0; i < allTasks.length; i++) {
-		const task = allTasks[i];
+  for (let i = 0; i < allTasks.length; i++) {
+    const task = allTasks[i];
 
-		if (task['cardContainer'] == 'to-do-container') {
-			a++;
-		}
-		if (task['cardContainer'] == 'in-progress-container') {
-			b++;
-		}
-		if (task['cardContainer'] == 'await-feedback-container') {
-			c++;
-		}
-		if (task['cardContainer'] == 'done-container') {
-			d++;
-		}
-		if (task['prioName'] == 'urgent') {
-			u++;
-			if ((u = 1)) {
-				date = transformDate(i);
-			}
-		}
+    if (task["cardContainer"] == "to-do-container") {
+      a++;
+    }
+    if (task["cardContainer"] == "in-progress-container") {
+      b++;
+    }
+    if (task["cardContainer"] == "await-feedback-container") {
+      c++;
+    }
+    if (task["cardContainer"] == "done-container") {
+      d++;
+    }
+    if (task["prioName"] == "urgent") {
+      u++;
+      if ((u = 1)) {
+        date = changeDateFormat(i);
+      }
+    }
 
-		document.querySelector('.summaryToDo').innerHTML = a;
-		document.querySelector('.summaryDone').innerHTML = d;
-		document.querySelector('.summaryUrgent').innerHTML = u;
-		document.querySelector('.summaryDate').innerHTML = date;
-		document.querySelector('.summaryAllTasks').innerHTML = i + 1;
-		document.querySelector('.summaryProgress').innerHTML = b;
-		document.querySelector('.summaryFeedback').innerHTML = c;
-		document.querySelector('#greeting-name').innerHTML = 'AAA BBB';
-	}
+    document.querySelector(".summaryToDo").innerHTML = a;
+    document.querySelector(".summaryDone").innerHTML = d;
+    document.querySelector(".summaryUrgent").innerHTML = u;
+    document.querySelector(".summaryDate").innerHTML = changeDateFormat(i);
+    document.querySelector(".summaryAllTasks").innerHTML = i + 1;
+    document.querySelector(".summaryProgress").innerHTML = b;
+    document.querySelector(".summaryFeedback").innerHTML = c;
+    document.querySelector("#greeting-name").innerHTML = "AAA BBB";
+  }
+
+  function changeDateFormat(i) {
+    let currentDate = allTasks[i].date;
+    let parts = currentDate.split('-');
+    let year = parts[0];
+    let month = parseInt(parts[1], 10) - 1;
+    let day = parseInt(parts[2], 10);
+    let date = new Date(year, month, day);
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+    let formattedDate = monthNames[date.getMonth()] + ' ' + day + ', ' + date.getFullYear();
+    return formattedDate;
+  }
 }
