b515a74eb4b4d230df760f66c7c23477551671c3
Felix Wahls <felixwahls@gmx.de>
2024-04-15 00:18:28 +0200

    reworked slider animation on add-Task

--- js/add-task.js
+++ js/add-task.js
@@ -335,11 +335,6 @@ async function createTask(event) {
 
 	if (titleInput && dateInput && categoryInput) {
 		await pushTask();
-		if ((window.location.href = "../html/board.html")) {
-			await initBoard();
-		} else {
-			window.location.href = "../html/board.html";
-		}
 	}
 }
 
@@ -429,7 +424,18 @@ function showSlider() {
 		document
 			.querySelector(".task-added-slider")
 			.classList.add("task-added-transition-remove");
+		setTimeout(() => {
+			redirectToBoard();
+		}, 900);
 	}, 900);
+}
+
+async function redirectToBoard() {
+	if ((window.location.href = "../html/board.html")) {
+		await initBoard();
+	} else {
+		window.location.href = "../html/board.html";
+	}
 }
 
 /**
