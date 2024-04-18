c595023aed25270c522bf38e5e9e00e6441f2bfe
Felix Wahls <felixwahls@gmx.de>
2024-04-18 08:27:49 +0200

    reworked privacy and legal side if not logged in

--- js/legalNotice.js
+++ js/legalNotice.js
@@ -1,6 +1,13 @@
 async function initLegal() {
+	let prevPage = document.referrer;
 	await includeHTML();
-	showUserInitials();
+	if (prevPage.includes("logIn.html" || prevPage.includes("signUp.html"))) {
+		document.getElementById("menu-items").classList.add("d-none");
+		document.getElementById("header-icons").classList.add("d-none");
+		document.getElementById("menu-items-resp").classList.add("d-none");
+	} else {
+		showUserInitials();
+	}
 }
 
 function historyBack() {
